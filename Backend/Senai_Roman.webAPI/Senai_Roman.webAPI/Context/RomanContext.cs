e1508303e3d5f7da8d52fa9804d890d6857f187c
Samuel <samuel.figs@gmail.com>
2021-11-29 10:26:23 -0300

    inicioMobileOK

--- Backend/Senai_Roman.webAPI/Senai_Roman.webAPI/Context/RomanContext.cs
+++ Backend/Senai_Roman.webAPI/Senai_Roman.webAPI/Context/RomanContext.cs
@@ -28,7 +28,9 @@ namespace Senai_Roman.webAPI.Context
             if (!optionsBuilder.IsConfigured)
             {
 //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=NOTE0113I2\\SQLEXPRESS; Initial Catalog=Roman; user id=sa; pwd=Senai@132;");
+                //optionsBuilder.UseSqlServer("Data Source=NOTE0113I2\\SQLEXPRESS; Initial Catalog=Roman; user id=sa; pwd=Senai@132;");
+                optionsBuilder.UseSqlServer("Data Source=PANZERII\\SQLEXPRESS; Initial Catalog=Roman; user id=sa; pwd=senai@#132;");
+                
             }
         }
 
