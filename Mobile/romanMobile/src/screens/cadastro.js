520025903321a99225cf9af71bb0346fca1f36e4
unknown <33848821800>
2021-12-01 17:12:56 -0300

    cadastro_sugestao

--- Mobile/romanMobile/src/screens/cadastro.js
+++ Mobile/romanMobile/src/screens/cadastro.js
@@ -1,5 +1,11 @@
 import React, { Component } from "react";
 import { Picker } from '@react-native-picker/picker';
+// import RNPickerSelect from 'react-native-picker-select';
+
+import api from '../services/api';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import {
     StyleSheet,
@@ -7,10 +13,8 @@ import {
     Text,
     TextInput,
     TouchableOpacity,
-    FlatList
+    FlatList,
 } from 'react-native';
-
-//import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default class Cadastro extends Component {
 
@@ -19,19 +23,59 @@ export default class Cadastro extends Component {
         this.state = {
             titulo: '',
             descricao: '',
+            idTema: 0,
             listaTemas: [],
         };
     }
 
-    buscarEventos = async () => {
-        const resposta = await api.get('/Temas');
-        // console.warn(resposta);
-        const dadosDaApi = resposta.data;
-        this.setState({ listaTemas: dadosDaApi });
+    buscarTemas = async () => {
+        try {
+            // const Token = await AsyncStorage.getItem('userToken');
+
+            const resposta = await api.get('/Temas', {
+                headers: {
+                    Authorization: 'Bearer ' + "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNhdWxvQGVtYWlsLmNvbSIsImp0aSI6IjIiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiIyIiwicm9sZSI6IjIiLCJleHAiOjE2MzgzODczNzYsImlzcyI6IlNlbmFpX1JvbWFuLndlYkFQSSIsImF1ZCI6IlNlbmFpX1JvbWFuLndlYkFQSSJ9.pGHq68HH5AWYZAZ3SP0h37alSKeFMccI4n21Q5-qqBo"
+                },
+            });
+            console.warn(resposta);
+            const dadosDaApi = await resposta.data;
+            this.setState({ listaTemas: dadosDaApi });
+
+        } catch (error) {
+            console.warn(error)
+        }
+
+
+
     };
 
+    Cadastrar = async () => {
+        try {
+            const resposta = await api.post('/Sugestaos', {
+                "idTema": this.state.idTema,
+                "tituloSugestao": this.state.titulo,
+                "descricao": this.state.descricao
+            }, {
+                headers: {
+                    Authorization: 'Bearer ' + "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNhdWxvQGVtYWlsLmNvbSIsImp0aSI6IjIiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiIyIiwicm9sZSI6IjIiLCJleHAiOjE2MzgzODg3NzgsImlzcyI6IlNlbmFpX1JvbWFuLndlYkFQSSIsImF1ZCI6IlNlbmFpX1JvbWFuLndlYkFQSSJ9.6ja63DhYb55rmG34X5wzSVVYdm-XQB4GvduUBcFRjmY"
+                },
+            });
+            console.warn(resposta);
+        } catch (error) {
+            console.warn(error)
+        }
+    }
+
     componentDidMount() {
-        this.buscarEventos();
+        this.buscarTemas();
+    }
+
+    encontrarTemas = () => {
+        return (
+            this.state.listaTemas.map((tema) => {
+                return { key: tema.idTema, label: { tituloTema }, value: tema.idTema }
+            })
+        )
     }
 
     render() {
@@ -40,64 +84,85 @@ export default class Cadastro extends Component {
                 <View style={styles.box}>
                     <View style={styles.dados}>
                         <Text>Título</Text>
-                        <TextInput style={styles.inserir} onValueChange={this.state.titulo} />
+                        <TextInput style={styles.inserir} onChangeText={titulo => this.setState({ titulo })} />
                     </View>
                     <View style={styles.dados}>
                         <Text>Descrição</Text>
-                        <TextInput style={styles.inserir} onValueChange={this.state.descricao} />
+                        <TextInput style={styles.inserir} onChangeText={descricao => this.setState({ descricao })} />
                     </View>
                     <View style={styles.dados}>
                         <Text>Tema</Text>
-                        <FlatList
-                            contentContainerStyle={styles.mainBodyContent}
-                            data={this.state.listaTemas}
-                            keyExtractor={item => item.idTema}
-                            renderItem={this.renderItem}
-                        />
 
 
-                        {/* <Picker
-
-                            style={{ height: 50, width: 150 }}
-
+                        <Picker
+                            selectedValue={this.state.idTema}
+                            onValueChange={(itemValue) => this.setState({ idTema: itemValue })}
                         >
-                            <Picker.Item label="Java" value="java" />
-                            <Picker.Item label="JavaScript" value="js" />
+                            <Picker.Item value={0} label={'Selecione um valor'} key={0} />
+                            {
+                                this.state.listaTemas.map((escopo) => {
+                                    return <Picker.Item value={escopo.idTema} label={escopo.tituloTema} key={escopo.idTema} />
+                                })
+                            }
                         </Picker>
 
 
 
-
-
-
-                        <Picker
-                            selectedValue={selectedValue}
-                            onValueChange={(itemValue) => setSelectedValue(itemValue)}>
-                            {
-                                this.state.listaTemas.map((escopo) => {
-                                    return <Picker.Item value={escopo.tituloTema} label='Título' key={escopo.idTema} />
-                                })
-                            }
-                        </Picker> */}
-
                     </View>
-                    <TouchableOpacity style={styles.btnCadastar}>
+                    <TouchableOpacity style={styles.btnCadastar} onPress={this.Cadastrar}>
                         <Text>Cadastrar</Text>
                     </TouchableOpacity>
                 </View>
+
+                <bottomTab.Navigator
+                    initialRouteName='Home'
+
+                    screenOptions={({ route }) => ({
+                        tabBarIcon: () => {
+                            if (route.name === 'Home') {
+                                return (
+                                    <Image
+                                        source={require('../../Assets/IconsNavigation/home.png')}
+                                        style={styles.tabBarIcon}
+                                    />
+                                )
+                            }
+                            if (route.name === 'Escopos') {
+                                return (
+                                    <Image
+                                        source={require('../../Assets/IconsNavigation/escopos.png')}
+                                        style={styles.tabBarIcon}
+                                    />
+                                )
+                            }
+                        },
+
+                        headerShown: false,
+                        tabBarShowLabel: false,
+                        tabBarActiveBackgroundColor: '#EDB205',
+                        tabBarInactiveBackgroundColor: '#EDB205',
+                        tabBarActiveTintColor: 'red',
+                        tabBarInactiveTintColor: '#FFFFFF',
+                    })}
+                >
+                    <bottomTab.Screen name="Home" component={Home} />
+                    <bottomTab.Screen name="Escopos" component={Escopos} />
+
+                </bottomTab.Navigator>
+
             </View>
         );
     }
 
-    renderItem = ({ item }) => (
-        // <Text style={{ fontSize: 20, color: 'red' }}>{item.nomeEvento}</Text>
+    // renderItem = ({ item }) => (
+    //     // <Text style={{ fontSize: 20, color: 'red' }}>{item.nomeEvento}</Text>
 
-        <View style={styles.flatItemRow}>
+    //     <View style={styles.flatItemRow}>
 
-            <Text style={styles.flatItemTitle}>{item.tituloTema}</Text>
+    //         <Text style={styles.flatItemTitle}>{item.tituloTema}</Text>
 
-        </View>
-    );
+    //     </View>
+    // );
 
 }
 
@@ -136,22 +201,6 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     },
-    mainBodyContent: {
-        // paddingTop: 30,
-        // paddingRight: 50,
-        // paddingLeft: 50,
-    },
-    // dados do evento de cada item da lista (ou seja, cada linha da lista)
-    flatItemRow: {
-        // flexDirection: 'row',
-        // borderBottomWidth: 1,
-        // borderBottomColor: '#ccc',
-        // marginTop: 40,
-    },
 
-    flatItemTitle: {
-        fontSize: 16,
-        color: '#333',
-    },
 
 })
--- Mobile/romanMobile/src/services/api.js
+++ Mobile/romanMobile/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    //baseURL: 'http://192.168.3.103:5000/api' //Samuel_F1
-    baseURL: 'http://192.168.0.103:5000/api' 
+    baseURL: 'http://192.168.3.103:5000/api' //Samuel_F1
+    //baseURL: 'http://192.168.0.103:5000/api' 
 });
 
 export default api;
